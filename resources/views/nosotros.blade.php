4761afac347804e08b8b031bac8f52bb1ab020f8
Roberto Hodgson A <robertohodgson2000@gmail>
2023-02-26 23:29:48 -0600

    proyecto inicial Devstagram

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Nostros</title>
+    </head>
+    <body>
+       <h1>Nosotros</h1>
+
+       <nav>
+            <a href="/">Principal</a>
+            <a href="/nosotros">Nosotros</a>
+        </nav>
+    </body>
+</html>
